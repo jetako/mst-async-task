3e19eb86ecf6565339ac12d5d5e4baab615114e2
Jesse Cooper <jesse@iterate.co>
2021-03-10 14:59:07 -0800

    fixed missing README and LICENSE in npm package, removed sourcemaps

--- packages/mst-async-task/rollup.config.js
+++ packages/mst-async-task/rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from 'rollup-plugin-node-resolve'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 
 const pkg = require('./package.json')
 
@@ -12,12 +11,11 @@ export default {
   input: 'build/index.js',
   external: ['mobx-state-tree', 'tslib'],
   output: [
-    { file: pkg.main, format: 'cjs', globals, sourcemap: true },
-    { file: pkg['umd:main'], name: 'mstAsyncTask', format: 'umd', globals, sourcemap: true },
-    { file: pkg.module, format: 'es', globals, sourcemap: true },
+    { file: pkg.main, format: 'cjs', globals },
+    { file: pkg['umd:main'], name: 'mstAsyncTask', format: 'umd', globals },
+    { file: pkg.module, format: 'es', globals },
   ],
   plugins: [
-    resolve(),
-    sourceMaps(),
+    resolve()
   ]
 }
